9909e5a6f2a6ed4f1cdf4a2384eaca350c137179
Ivan <cnyazev.ivan@yandex.ru>
2021-01-31 13:02:58 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isPoint = false;
+        public bool isOperation = false;
+
+        private string num1 = null;
+        private string num2 = null;
+
+        private string currentOperation = "";      
+
+        private void buttonNumberClick(object obj, EventArgs e)
+        {
+            var txt = ((Button)obj).Text;
+            if (isPoint && txt == ",") { return; }
+
+            if (isOperation) { 
+                num2+= txt;
+                textBoxResult.Text = num2;
+            } 
+            else {
+                num1 += txt;
+                textBoxResult.Text=num1; 
+            }
+
+            if(txt == ",") { isPoint = true; }
+           
+        }
+    
+        private void OutputResult(string result, bool isSingle, string operation)
+        {
+            if(result == null) { return;}
+
+            if (!isSingle) //Если оперируем с двумя числами 
+            {
+                textBoxHistory.Text = num1 + " " + operation + " " + num2+" = ";
+                num1 = result; //Записываем в первое число полученный результат
+                num2 = null; //Онулируем второе число
+            }
+            else 
+            {
+                textBoxHistory.Text = operation + num1 + " = ";
+                isOperation = false;
+                num1 = null;
+                num2 = null;
+            }
+            textBoxResult.Text = result; //Выводим результат               
+        }    
+        private void ChoiceOperation(string operation)
+        {
+            string result = null;
+            bool isSingleOperation = false; //Работаем с одним числом или двумя
+            
+            if(num1 == null) { num1 = textBoxResult.Text;}
+            textBoxHistory.Text=num1+" "+operation+" ";
+            
+            switch (operation)
+            {
+                case "+": { result =  MathOperation.Add(num1,num2); break; }
+                case "-": { result =  MathOperation.Sub(num1,num2); break; }
+                case "x": { result =  MathOperation.Mul(num1,num2); break; }
+                case "÷": { result =  MathOperation.Dev(num1,num2); break; }
+                case "√": { result =  MathOperation.Sqr(num1); isSingleOperation = true; isOperation = false; break; }
+                case "x²": { result =  MathOperation.Pow(num1); isSingleOperation = true; isOperation = false; break; }
+                case "1/x": { result =  MathOperation.OneDev(num1); isSingleOperation = true; isOperation = false; break; }
+                default: break;
+            }
+                OutputResult(result,isSingleOperation, operation);
+                isPoint = false; //Разрешаем ставить запятую
+        }
+        private void buttonOperationClick(object obj, EventArgs e)
+        {
+            isOperation = true;
+            currentOperation = ((Button)obj).Text;
+            ChoiceOperation(((Button)obj).Text);
+             
+        }
+        //button "="
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChoiceOperation(currentOperation);
+        }
+
+        //button clear
+        private void button22_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Text = "0";
+            textBoxHistory.Text = "";
+            isOperation = false;
+            num1 = null;
+            num2 = null;
+            isPoint = false;
+        }
+    }
+}
---
+++ WindowsFormsApp2/MathOperation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+namespace WindowsFormsApp2
+{
+   public static class MathOperation
+    {
+        private static double ConvertToDouble(string numStr)
+        {
+            double number;
+            try
+            {
+                number = Convert.ToDouble(numStr);
+            }
+            catch (Exception)
+            {
+
+                number = 0;
+            }
+            return number;
+        }
+        public static string Add(string num1, string num2)
+        {
+            if(num1==null || num2 == null) { return null;}
+            double result = ConvertToDouble(num1) + ConvertToDouble(num2);
+            return result.ToString();
+        }
+
+        public static string Sub(string num1, string num2)
+        {
+            if (num1 == null || num2 == null) { return null; }
+            double result = ConvertToDouble(num1) - ConvertToDouble(num2);
+            return result.ToString();
+        }
+
+        public static string Mul(string num1, string num2)
+        {
+            if (num1 == null || num2 == null) { return null; }
+            double result = ConvertToDouble(num1) * ConvertToDouble(num2);
+            return result.ToString();
+        }
+
+        public static string Dev(string num1, string num2)
+        {
+            if (num1 == null || num2 == null) { return null; }
+            double b = ConvertToDouble(num2);
+            if (b == 0) { return "Попытка деления на 0!";}
+            return (ConvertToDouble(num1)/b).ToString();
+        }
+
+        public static string Sqr(string num1)
+        {
+            if (num1 == null) { return null; }
+            double b = ConvertToDouble(num1);
+            b = Math.Sqrt(b);
+            return b.ToString();
+        }
+        public static string Pow(string num1)
+        {
+            if (num1 == null) { return null; }
+            double b = ConvertToDouble(num1);
+            return (b*b).ToString();
+
+        }
+
+        public static string OneDev(string num1)
+        {
+            if (num1 == null) { return null; }
+            double b = ConvertToDouble(num1);
+            if(b == 0) { return "Попытка деления на 0!";}
+            return (1/b).ToString();
+        }
+    }
+}
